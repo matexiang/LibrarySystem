21c72d4f698fad5b548b3b9a3efebc8f27c12919
matexiang <matexy2024@126.com>
2024-12-12 12:37:22 +0800

    config类

---
+++ src/main/java/com/lxy/book/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.lxy.book.config;
+
+import com.lxy.book.interceptor.LoginInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Autowired
+    private LoginInterceptor loginInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/user/login")
+                .excludePathPatterns("/css/**")
+                .excludePathPatterns("/js/**")
+                .excludePathPatterns("/pic/**")
+                .excludePathPatterns("/**/*.html")
+                .excludePathPatterns("/test/**");
+    }
+}
